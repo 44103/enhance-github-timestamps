50e6ede151895a34ef10f0c9aecf4a267587fef6
Kenko Yoshitomi <kyoshitomi.cursor@gmail.com>
2025-02-15 14:35:53 +0900

    feat: implement abstract time

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <>
       <div className="container w-[25rem] p-1 bg-zinc-800">
-        <div className="text-white">Chrome Extension!</div>
+        <div className="text-white">GitHub DateTime Formatter</div>
       </div>
     </>
   );
